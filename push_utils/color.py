fa2d2fe64ff124062c43577d9ac16c5b5cf56719
Yumpty <julian.hemmer@epitech.eu>
2025-04-06 20:19:26 +0200

    [✅] - Add the collor to the color.py in github_utils.

--- push_utils/color.py
+++ push_utils/color.py
@@ -5,9 +5,22 @@
 
 RESET = 0
 BOLD = 1
+FAINT = 2
+ITALIC = 3
 UNDERLINE = 4
 BLINK = 5
 REVERSE = 7
+STRIKE_THROUGH = 9
 
-def c(text, color = 0, modifier = RESET):
-    return f"\033[{modifier};{color}m{text}\033[0m"
+def c(text, fg=15, modifier=None, bg=None):
+    seq = []
+    if isinstance(modifier, list):
+        seq.extend(str(m) for m in modifier)
+    elif modifier is not None:
+        seq.append(str(modifier))
+    seq.append(f"38;5;{fg}")
+
+    if bg is not None:
+        seq.append(f"48;5;{bg}")
+
+    return f"\033[{';'.join(seq)}m{text}\033[0m"
